d488f9058d8d9ba00fca5ff659bf3979dd3463ea
Peter <peter.olofsson@plockmatic.com>
2022-08-11 15:24:24 +0200

    ProgressBar pushes div

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 import KompA from './components/KompA.vue'
 import KompB from './components/KompB.vue'
 import KompC from './components/KompC.vue'
+import ProgressBar from './components/ProgressBar.vue'
 const name = "Peter"
 </script>
 
@@ -15,7 +16,7 @@ const name = "Peter"
     </div>
   </header>
 
-  <main>
+  <main class="flex">
     <div class="text-1xl text-green-600 font-bold px-2 py-4">
       <KompA />
       <KompB var-med-camel="1+2" />
@@ -26,6 +27,9 @@ const name = "Peter"
       <div class="border border-yellow-300">
         <KompB v-for="item in vehicles" :var-med-camel="item" />
       </div>
+      <ProgressBar :progress="b % 15" />
+    </div>
+    <div class="bg-slate-300 w-32 h-32">
     </div>
     <button class="fixed top-5 left-60 w-8 h-8 bg-orange-500 rounded-full" @click="b++;">+</button>
   </main>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Progress: {{ ''.padEnd(progress,'#') }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['progress']
+}
+</script>
