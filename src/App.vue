6ee1a57d8f607b0cad03b6d4fbd2e01c01f55378
Peter Olofsson <peter.olofsson@plockmatic.se>
2022-08-11 12:37:29 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Tjena" />
+    </div>
+  </header>
+
+  <main>
+    <h1 class="text-3xl text-green-600 font-bold underline">
+      Hello där!
+    </h1>
+  </main>
+</template>
