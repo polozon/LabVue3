4abd7009f6ad3371354b9486ba024c260ad02ca8
Peter <peter.olofsson@plockmatic.com>
2022-08-11 16:29:11 +0200

    Round buttons with emit to parent

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import KompA from './components/KompA.vue'
 import KompB from './components/KompB.vue'
 import KompC from './components/KompC.vue'
 import ProgressBar from './components/ProgressBar.vue'
+import RoundButton from './components/RoundButton.vue'
 const name = "Peter"
 </script>
 
@@ -34,6 +35,8 @@ const name = "Peter"
       <!-- Building the class with v-bind -->
       <div v-for="col in colors" :class="col + ' w-12 h-12'" />
     </div>
+    <RoundButton color="bg-red-500" sign="-" @clicked="b--" />
+    <RoundButton color="bg-blue-500" sign="+" @clicked="b++" />
     <button class="fixed top-5 left-60 w-8 h-8 bg-orange-500 rounded-full" @click="b++;">+</button>
   </main>
 </template>
---
+++ src/components/RoundButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <button :class="color + ' w-10 h-10 m-1 rounded-full text-2xl font-bold'" @click="$emit('clicked')">{{ sign }}</button>
+</template>
+
+<script>
+export default {
+    props: ['color','sign']
+}
+</script>
