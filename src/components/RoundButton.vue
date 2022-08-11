f18dcb187cfcb8a3815edfd9173dca57d9c3a4f6
Peter <peter.olofsson@plockmatic.com>
2022-08-11 16:44:34 +0200

    Proper way to build the class

--- src/components/RoundButton.vue
+++ src/components/RoundButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :class="color + ' w-10 h-10 m-1 rounded-full text-2xl font-bold'" @click="$emit('clicked')">{{ sign }}</button>
+    <button :class="color" class="w-10 h-10 m-1 rounded-full text-2xl font-bold" @click="$emit('clicked')">{{ sign }}</button>
 </template>
 
 <script>
